a0177b2d61d1593880cd7e5a5c2b5eabfa2e853b
Il Harper <afanyiyu@hotmail.com>
2020-08-02 21:41:32 +0800

    feat: Add USBProvider

---
+++ Providers/USBProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyTap.Providers
+{
+    public sealed class USBProvider : IKeyTapProvider
+    {
+        #region Const Data
+
+        public string Name { get; } = "USB";
+
+        public string Icon { get; } = "ConnectPlugged";
+
+        #endregion
+
+        #region Key Events
+
+        public event EventHandler<TapKey> KeyDown;
+        public event EventHandler<TapKey> KeyUp;
+
+        #endregion
+
+        #region Dispose
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
