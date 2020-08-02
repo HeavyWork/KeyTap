18c409ba8d4036caefd51eb7581b899d0fa965d9
Il Harper <afanyiyu@hotmail.com>
2020-08-02 10:38:49 +0800

    refactor: Rename ITapKey to Class

---
+++ TapKey.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyTap
+{
+    public class TapKey
+    {
+        #region Data
+
+        string Type { get; }
+
+        string Device { get; }
+
+        string Id { get; }
+
+        string Name { get; }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"{Type}.{Device}.{Id}";
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+        #endregion
+    }
+}
