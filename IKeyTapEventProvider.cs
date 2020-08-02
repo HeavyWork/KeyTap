bc8bec675e98e9e5430f7367aa29e6569d63cac0
Il Harper <afanyiyu@hotmail.com>
2020-08-02 11:06:50 +0800

    feat: Add IKeyTapEventProvider Interface

---
+++ IKeyTapEventProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyTap
+{
+    public interface IKeyTapEventProvider
+    {
+
+        public event EventHandler<TapKey> KeyDown;
+
+        public event EventHandler<TapKey> KeyUp;
+
+    }
+}
